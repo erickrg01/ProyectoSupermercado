28fca12daa2cd9551750275325244c7befd5fe0a
vespertino <vespertino@localhost>
2024-05-10 17:34:09 +0200

    Commit con contraseña encriptada y fichero de lectura de contraseñas

--- ProyectoProgramacion/src/main/Supermercado.java
+++ ProyectoProgramacion/src/main/Supermercado.java
@@ -1,14 +1,18 @@
 package main;
 
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
@@ -60,37 +64,80 @@ public class Supermercado extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-	    String usuarioGuardado = "Yo";
-        String contraseñaGuardada = "12345";
 
-        String usuarioIngresado = JOptionPane.showInputDialog(null, "Ingrese su usuario:");
-        String contraseñaIngresada = JOptionPane.showInputDialog(null, "Ingrese su contraseña:");
+		String fileName = "fichero/contraseñas.txt";
 
-      
-        if (usuarioIngresado != null && contraseñaIngresada != null &&
-                usuarioIngresado.equals(usuarioGuardado) && contraseñaIngresada.equals(contraseñaGuardada)) {
-           
-            EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    try {
-                        Supermercado frame = new Supermercado();
-                        frame.setVisible(true);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } else {
-            
-            JOptionPane.showMessageDialog(null, "Credenciales incorrectas. El programa se cerrará.");
-            System.exit(0);
-        }
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String[] contraseñas = new String[4];
+
+			for (int i = 0; i < 4; i++) {
+				contraseñas[i] = bufferedReader.readLine();
+			}
+
+			bufferedReader.close();
+
+			String usuarioGuardado = "Yo";
+			String[] contraseñaGuardada = contraseñas;
+
+			String usuarioIngresado = JOptionPane.showInputDialog(null, "Ingrese su usuario:");
+
+			// Crear y configurar el JPasswordField
+			JPasswordField passwordField = new JPasswordField();
+			passwordField.setEchoChar('*');
+
+			// Panel para contener el JPasswordField
+			JPanel panel = new JPanel();
+			panel.setLayout(new GridLayout(2, 2));
+			panel.add(new JLabel("Ingrese su contraseña:"));
+			panel.add(passwordField);
+
+			// Mostrar el panel en un diálogo de opción
+			int option = JOptionPane.showConfirmDialog(null, panel, "Autenticación", JOptionPane.OK_CANCEL_OPTION);
+
+			if (option == JOptionPane.OK_OPTION) {
+				char[] passwordChars = passwordField.getPassword();
+				String contraseñaIngresada = new String(passwordChars);
+
+				boolean credencialesCorrectas = false;
+
+				for (String contraseña : contraseñaGuardada) {
+					if (usuarioIngresado != null && contraseñaIngresada != null &&
+							usuarioIngresado.equals(usuarioGuardado) && contraseñaIngresada.equals(contraseña)) {
+						credencialesCorrectas = true;
+						break;
+					}
+				}
+
+				if (credencialesCorrectas) {
+					EventQueue.invokeLater(() -> {
+						try {
+							Supermercado frame = new Supermercado();
+							frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					});
+				} else {
+					JOptionPane.showMessageDialog(null, "Credenciales incorrectas. El programa se cerrará.");
+					System.exit(0);
+				}
+			} else {
+				System.exit(0);
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error al leer el archivo: " + e.getMessage());
+		}
 	}
+
+
 
 	/**
 	 * Create the frame.
 	 */
-	
+
 	public Supermercado() {
 		crearGUI();
 		crearEventos();
@@ -105,39 +152,39 @@ public class Supermercado extends JFrame {
 		contentPane.setLayout(null);
 		tabbedPane.setBounds(12, 0, 1100, 650);
 		contentPane.add(tabbedPane);
-		
+
 		JPanel Inicio = new JPanel();
 		tabbedPane.addTab("Inicio", null, Inicio, null);
 		Inicio.setLayout(null);
-		
+
 		btnCarne = new JButton("Carne");
 		btnCarne.setBounds(83, 109, 117, 25);
 		Inicio.add(btnCarne);
-		
+
 		btnPescado = new JButton("Pescado");
 		btnPescado.setBounds(212, 109, 117, 25);
 		Inicio.add(btnPescado);
-		
+
 		btnVerdura = new JButton("Verdura");
 		btnVerdura.setBounds(341, 109, 117, 25);
 		Inicio.add(btnVerdura);
-		
+
 		btnFruta = new JButton("Fruta");
 		btnFruta.setBounds(470, 109, 117, 25);
 		Inicio.add(btnFruta);
-		
+
 		btnBebidas = new JButton("Bebidas");
 		btnBebidas.setBounds(599, 109, 117, 25);
 		Inicio.add(btnBebidas);
-		
+
 		btnVerFactura = new JButton("Ver factura");
 		btnVerFactura.setBounds(341, 242, 117, 25);
 		Inicio.add(btnVerFactura);
-		
+
 		pCarne = new JPanel();
 		tabbedPane.addTab("Carne", null, pCarne, null);
 		pCarne.setLayout(null);
-		
+
 		pPescado = new JPanel();
 		tabbedPane.addTab("Pescado", null, pPescado, null);
 		pPescado.setLayout(null);
@@ -149,61 +196,61 @@ public class Supermercado extends JFrame {
 		pFruta = new JPanel();
 		tabbedPane.addTab("Fruta", null, pFruta, null);
 		pFruta.setLayout(null);
-		
+
 		btnTernera = new JButton("Ternera");
 		btnTernera.setBounds(281, 86, 117, 25);
 		pCarne.add(btnTernera);
-		
+
 		txKilogramos = new JTextField();
 		txKilogramos.setBounds(391, 148, 114, 19);
 		pCarne.add(txKilogramos);
 		txKilogramos.setColumns(10);
-		
+
 		JLabel lblKg = new JLabel("Kg");
 		lblKg.setBounds(344, 149, 29, 17);
 		pCarne.add(lblKg);
-		
+
 		btnPollo = new JButton("Pollo");
 		btnPollo.setBounds(501, 86, 117, 25);
 		pCarne.add(btnPollo);
-		
+
 		btnVolver1 = new JButton("Volver");
 		btnVolver1.setBounds(388, 219, 117, 25);
 		pCarne.add(btnVolver1);
-		
+
 		btnVolverPescado = new JButton("Volver");
 		btnVolverPescado.setBounds(388, 219, 117, 25);
 		pPescado.add(btnVolverPescado);
-		
+
 		btnVolverVerdura = new JButton("Volver");
 		btnVolverVerdura.setBounds(388, 219, 117, 25);
 		pVerdura.add(btnVolverVerdura);
-		
+
 		btnVolverFruta = new JButton("Volver");
 		btnVolverFruta.setBounds(388, 219, 117, 25);
 		pFruta.add(btnVolverFruta);
-		
+
 		pBebidas = new JPanel();
 		tabbedPane.addTab("Bebidas", null, pBebidas, null);
 		pBebidas.setLayout(null);
-		
+
 		btnAgua = new JButton("Agua");
 		btnAgua.setBounds(282, 102, 117, 25);
 		pBebidas.add(btnAgua);
-		
+
 		btnVolver2 = new JButton("Volver 2");
 		btnVolver2.setBounds(402, 232, 117, 25);
 		pBebidas.add(btnVolver2);
-		
+
 		txNumBotellas = new JTextField();
 		txNumBotellas.setBounds(384, 172, 114, 19);
 		pBebidas.add(txNumBotellas);
 		txNumBotellas.setColumns(10);
-		
+
 		lblNumeroDeBotellas = new JLabel("Numero de Botellas");
 		lblNumeroDeBotellas.setBounds(202, 174, 149, 15);
 		pBebidas.add(lblNumeroDeBotellas);
-		
+
 		//FOTOS
 		tabbedPane.addTab("Carne", null, pCarne, null);
 		pCarne.setLayout(null);
@@ -244,41 +291,41 @@ public class Supermercado extends JFrame {
 		lblFondoBebidas.setBounds(0, 0, 1100, 650);
 		lblFondoBebidas.setIcon(new ImageIcon("imagenes_supermercado/bebidas.jpg")); 
 		pBebidas.add(lblFondoBebidas);
-		
+
 		JPanel pFactura = new JPanel();
 		tabbedPane.addTab("Factura", null, pFactura, null);
 		pFactura.setLayout(null);
-		
+
 		btnQuitarProducto = new JButton("Quitar producto");
 		btnQuitarProducto.setBounds(714, 90, 146, 25);
 		pFactura.add(btnQuitarProducto);
-		
+
 		txNombreProducto = new JTextField();
 		txNombreProducto.setBounds(546, 93, 114, 19);
 		pFactura.add(txNombreProducto);
 		txNombreProducto.setColumns(10);
-		
+
 		btnFinalizarCompra = new JButton("Finalizar compra");
 		btnFinalizarCompra.setBounds(701, 230, 159, 25);
 		pFactura.add(btnFinalizarCompra);
-		
+
 		txFactura = new JTextPane();
 		txFactura.setEditable(false);
 		txFactura.setBounds(42, 31, 492, 529);
 		pFactura.add(txFactura);
-		
+
 		btnVolverAInicio = new JButton("Volver a inicio");
 		btnVolverAInicio.setBounds(701, 158, 159, 25);
 		pFactura.add(btnVolverAInicio);
-		
+
 	}
 	public void crearEventos() {
 		btnCarne.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabbedPane.setSelectedIndex(1);
-            }
-        });
-		
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedIndex(1);
+			}
+		});
+
 		btnPescado.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tabbedPane.setSelectedIndex(2);
@@ -295,48 +342,48 @@ public class Supermercado extends JFrame {
 			}
 		});
 		btnBebidas.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabbedPane.setSelectedIndex(5);
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedIndex(5);
+			}
+		});
 		btnVerFactura.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	txFactura.setText(nuevaFactura.mostrarListaCompra());
-                tabbedPane.setSelectedIndex(6);
-                String fileName = "data/salida.txt";
+			public void actionPerformed(ActionEvent e) {
+				txFactura.setText(nuevaFactura.mostrarListaCompra());
+				tabbedPane.setSelectedIndex(6);
+				String fileName = "data/salida.txt";
 
-                try {
-                    // Crear un BufferedWriter para escribir en el archivo
-                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
+				try {
+					// Crear un BufferedWriter para escribir en el archivo
+					BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName));
 
-                    String input = nuevaFactura.mostrarListaCompra();
-                    bufferedWriter.write(input);
-                    // Cerrar el BufferedWriter y el Scanner
-                    bufferedWriter.close();
-                    
-                    System.out.println("Las cadenas han sido escritas en el archivo '" + fileName + "'.");
-                } catch (IOException excp) {
-                    System.out.println("Error al escribir en el archivo: " + excp.getMessage());
-                }
-            }
-        });
+					String input = nuevaFactura.mostrarListaCompra();
+					bufferedWriter.write(input);
+					// Cerrar el BufferedWriter y el Scanner
+					bufferedWriter.close();
+
+					System.out.println("Las cadenas han sido escritas en el archivo '" + fileName + "'.");
+				} catch (IOException excp) {
+					System.out.println("Error al escribir en el archivo: " + excp.getMessage());
+				}
+			}
+		});
 		btnVolverAInicio.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabbedPane.setSelectedIndex(0);
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedIndex(0);
+			}
+		});
 		btnVolver1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabbedPane.setSelectedIndex(0);
-            }
-        });
-		
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedIndex(0);
+			}
+		});
+
 		btnVolver2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tabbedPane.setSelectedIndex(0);
 			}
 		});
-		
+
 		btnVolverPescado.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tabbedPane.setSelectedIndex(0);
@@ -352,38 +399,38 @@ public class Supermercado extends JFrame {
 				tabbedPane.setSelectedIndex(0);
 			}
 		});
-			
+
 		btnTernera.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	double precio = 10.49;
-                double peso = Double.parseDouble(txKilogramos.getText());
-                ProductoFresco pf = new ProductoFresco("Ternera",precio,peso);
-                nuevaFactura.añadirProducto(pf);
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+				double precio = 10.49;
+				double peso = Double.parseDouble(txKilogramos.getText());
+				ProductoFresco pf = new ProductoFresco("Ternera",precio,peso);
+				nuevaFactura.añadirProducto(pf);
+			}
+		});
 		btnPollo.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	double precio = 8.75;
-                double peso = Double.parseDouble(txKilogramos.getText());
-                ProductoFresco pf = new ProductoFresco("Pollo",precio,peso);
-                nuevaFactura.añadirProducto(pf);
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+				double precio = 8.75;
+				double peso = Double.parseDouble(txKilogramos.getText());
+				ProductoFresco pf = new ProductoFresco("Pollo",precio,peso);
+				nuevaFactura.añadirProducto(pf);
+			}
+		});
 		btnAgua.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	double precio = 0.65;
-                int numBotellas = Integer.parseInt(txKilogramos.getText());
-                ProductoBebida pf = new ProductoBebida("Agua",precio,numBotellas);
-                nuevaFactura.añadirProducto(pf);
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+				double precio = 0.65;
+				int numBotellas = Integer.parseInt(txKilogramos.getText());
+				ProductoBebida pf = new ProductoBebida("Agua",precio,numBotellas);
+				nuevaFactura.añadirProducto(pf);
+			}
+		});
 		btnQuitarProducto.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				nuevaFactura.quitarProducto(txNombreProducto.getText());
 				txFactura.setText(nuevaFactura.mostrarListaCompra());
 			}
 		});
-		
+
 	}
 }
